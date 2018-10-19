086ad8fe921f8bbfb21b3e913e6a055f8f7bb3ec
Dennygod <dennygod1220@gmail.com>
2018-10-19 13:49:15 +0800

    2

--- download_site.js
+++ download_site.js
@@ -2,7 +2,7 @@ var scrape = require('website-scraper');
 
 var options = {
     urls: ["https://www.tagsis.com/"],
-    directory: '/tagsis_test',
+    directory: 'tagsis_test',
     request: {
         headers: {
             'content-type': 'charset=UTF-8',
