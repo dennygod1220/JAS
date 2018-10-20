083bf5fbc3f35ce6e92686208fc7e0f108131657
Dennygod <dennygod1220@gmail.com>
2018-10-20 16:05:10 +0800

    3

--- download_site.js
+++ download_site.js
@@ -1,16 +1,37 @@
 var scrape = require('website-scraper');
 
-var options = {
+var options_arr = [{
     urls: ["https://www.tagsis.com/"],
     directory: 'tagsis_test',
     request: {
         headers: {
             'content-type': 'charset=UTF-8',
         }
+    }
+}, {
+    urls: ["https://www.tagsis.com/"],
+    directory: 'tagsis_test2',
+    request: {
+        headers: {
+            'content-type': 'charset=UTF-8',
+        }
     },
-};
+}]
 
-scrape(options).then((result) => {
-    console.log(result);
+// var options = {
+//     urls: ["https://www.tagsis.com/"],
+//     directory: 'tagsis_test',
+//     request: {
+//         headers: {
+//             'content-type': 'charset=UTF-8',
+//         }
+//     },
+// };
+
+
+for(var i=0;i<options_arr.length;i++){
+    scrape(options_arr[i]).then((result) => {
+        console.log(result);
     
-}).catch(console.log);
+    }).catch(console.log);
+}
