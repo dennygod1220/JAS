d793ca39ea545233dddc53057d15aeb2b604a668
Dennygod <dennygod1220@gmail.com>
2018-10-20 16:12:34 +0800

    4

--- download_site.js
+++ download_site.js
@@ -1,22 +1,24 @@
 var scrape = require('website-scraper');
 
-var options_arr = [{
-    urls: ["https://www.tagsis.com/"],
-    directory: 'tagsis_test',
-    request: {
-        headers: {
-            'content-type': 'charset=UTF-8',
-        }
-    }
-}, {
-    urls: ["https://www.tagsis.com/"],
-    directory: 'tagsis_test2',
-    request: {
-        headers: {
-            'content-type': 'charset=UTF-8',
-        }
-    },
-}]
+
+
+var options_arr = [];
+
+function set_option(options_arr,url,dir){
+    var a = {
+        urls: [url],
+        directory: 'public/DemoPage/site/'+dir,
+        request: {
+            headers: {
+                'content-type': 'charset=UTF-8',
+            }
+        },
+    };
+    options_arr.push(a);
+}
+
+set_option(options_arr,"https://www.tagsis.com/",'tagsis1');
+set_option(options_arr,"https://www.tagsis.com/",'tagsis2');
 
 // var options = {
 //     urls: ["https://www.tagsis.com/"],
