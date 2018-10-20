6ee7419f70e3ad937ec0a3aa6579dbb9beab1bc1
Dennygod <dennygod1220@gmail.com>
2018-10-20 16:53:25 +0800

    13

--- download_site.js
+++ download_site.js
@@ -1,17 +1,23 @@
 var scrape = require('website-scraper');
 var fs = require('fs');
 var path = require("path");
-
+//儲存參數的陣列
 var options_arr = [];
 
-function set_option(options_arr,url,dir){
-    
-    if (fs.existsSync('public/DemoPage/site/'+dir)) {
-        deleteDir('public/DemoPage/site/'+dir);
+
+//指定下載的url和存放位置
+set_option(options_arr, "https://www.thenewslens.com/", 'tagsis1');
+set_option(options_arr, "https://www.tagsis.com/", 'tagsis2');
+
+function set_option(options_arr, url, dir) {
+    //如果該目錄已經存在，先將此目錄刪除
+    if (fs.existsSync('public/DemoPage/site/' + dir)) {
+        deleteDir('public/DemoPage/site/' + dir);
     }
+    //呼叫此function 後 會將 options 參數push進options 陣列
     var a = {
         urls: [url],
-        directory: 'public/DemoPage/site/'+dir,
+        directory: 'public/DemoPage/site/' + dir,
         request: {
             headers: {
                 'content-type': 'charset=UTF-8',
@@ -21,39 +27,37 @@ function set_option(options_arr,url,dir){
     options_arr.push(a);
 }
 
+//用for迴圈判斷 要下載多少東西
+for (var i = 0; i < options_arr.length; i++) {
+    scrape(options_arr[i]).then((result, options_arr) => {
 
-//指定下載的url和存放位置
-set_option(options_arr,"https://www.thenewslens.com/",'tagsis1');
-set_option(options_arr,"https://www.tagsis.com/",'tagsis2');
-
-
-for(var i=0;i<options_arr.length;i++){
-    scrape(options_arr[i]).then((result,options_arr) => {
-    
     }).catch(console.log);
 }
 
 
 
-function deleteDir(url){
+function deleteDir(url) {
     var files = [];
-        
-    if( fs.existsSync(url) ) {  //判断给定的路径是否存在
-           
-        files = fs.readdirSync(url);   //返回文件和子目录的数组
-        files.forEach(function(file,index){
-            var curPath = path.join(url,file);
-                
-            if(fs.statSync(curPath).isDirectory()) { //同步读取文件夹文件，如果是文件夹，则函数回调
+
+    if (fs.existsSync(url)) {
+        //判断给定的路径是否存在
+        files = fs.readdirSync(url);
+        //返回文件和子目录的数组
+        files.forEach(function (file, index) {
+            var curPath = path.join(url, file);
+
+            if (fs.statSync(curPath).isDirectory()) {
+                //同步读取文件夹文件，如果是文件夹，则函数回调
                 deleteDir(curPath);
-            } else {    
-                fs.unlinkSync(curPath);    //是指定文件，则删除
+            } else {
+                fs.unlinkSync(curPath);
+                //是指定文件，则删除
             }
-                
+
         });
-           
+
         fs.rmdirSync(url); //清除文件夹
-    }else{
+    } else {
         console.log("给定的路径不存在！");
     }
 
